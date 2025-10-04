ce4c8fa6b43fec854132e46fb0fb34930a5a6e8d
Anup Khismatrao <khismatrao2003@gmail.com>
2025-10-04 16:19:20 +0530

    Refactor: Securit config for managing CORS

--- src/main/java/com/payflow/app/security/config/SecurityConfig.java
+++ src/main/java/com/payflow/app/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.payflow.app.security.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.payflow.app.security.filter.JwtAuthenticationFilter;
 
@@ -25,6 +31,26 @@ public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthFilter;
 	private final UserDetailsService userDetailsService;
+	
+	@Value("${payflow.cors.allowed-origins}")
+    private String allowedOrigins;
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        // 1. Convert the comma-separated string from the property into a List
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+        
+        // 2. Configure other essential CORS settings
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
+        configuration.setAllowCredentials(true); 
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); 
+        return source;
+    }
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -46,7 +72,9 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
+		http
+		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+		.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**", // expose
 																													// login/signup
 						"/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/api/organizations/register").permitAll().anyRequest()
