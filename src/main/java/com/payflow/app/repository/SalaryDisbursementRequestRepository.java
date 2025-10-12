b2dd140e2377d1ab249633e378fefb7c218b62e1
Anup Khismatrao <khismatrao2003@gmail.com>
2025-10-12 22:50:53 +0530

    Refactor: Salary disburment service

--- src/main/java/com/payflow/app/repository/SalaryDisbursementRequestRepository.java
+++ src/main/java/com/payflow/app/repository/SalaryDisbursementRequestRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.payflow.app.entity.Organization;
 import com.payflow.app.entity.SalaryDisbursementRequest;
 import com.payflow.app.enums.DisbursementStatus;
 
 public interface SalaryDisbursementRequestRepository extends JpaRepository<SalaryDisbursementRequest, Long> {
     Page<SalaryDisbursementRequest> findByStatus(DisbursementStatus status, Pageable pageable);
+    Page<SalaryDisbursementRequest> findByOrganization(Organization organization, Pageable pageable);
+
+    Page<SalaryDisbursementRequest> findByOrganizationAndStatus(Organization organization, DisbursementStatus status, Pageable pageable);
+    
 }
